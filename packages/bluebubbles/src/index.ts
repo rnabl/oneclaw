7078be9b322b145ea7e915ddbfd0190c5adc7001
achisolana <ryan@nabl.ai>
2026-02-18 15:03:07 -0700

    chore: Remove old iMessage and ZeroClaw packages

--- packages/bluebubbles/src/index.ts
+++
@@ -1,5 +0,0 @@
-// @oneclaw/bluebubbles - BlueBubbles iMessage bridge client
-
-export * from './client';
-export * from './types';
-export * from './webhook';
--- packages/sendblue/src/index.ts
+++
@@ -1,5 +0,0 @@
-// @oneclaw/sendblue - Sendblue iMessage API client
-
-export * from './client';
-export * from './types';
-export * from './webhook';
